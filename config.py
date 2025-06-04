7fea3dbef97e759e7d0ee915bb310d6393994a50
irevivestore <mail@irevive-store.de>
2025-06-04 21:10:10 +0200

    config.py erstellen

---
+++ config.py
@@ -0,0 +1,17 @@
+# config.py
+
+REPARATURKOSTEN = {
+    "display": 80,
+    "akku": 30,
+    "backcover": 60,
+    "kamera": 100,
+    "lautsprecher": 60,
+    "mikrofon": 50,
+    "face id": 80,
+    "wasserschaden": 250,
+    "kein bild": 80,
+    "defekt": 0  # allgemeiner Hinweis
+}
+
+VERKAUFSPREIS = 500  # z. B. realistischer Verkaufspreis
+WUNSCH_MARGE = 120   # gewünschte Marge
